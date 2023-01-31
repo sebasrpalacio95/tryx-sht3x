bf4f5c08d211f69a5691742949b268a3724a3b3d
sebasrpalacio95 <sebastianrobledo95@gmail.com>
2023-01-31 14:58:33 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,47 +1,39 @@
-let humidity_checksum = 0
-let humidity_lsb = 0
-let humidity_msb = 0
-let temp_checksum = 0
-let temp_lsb = 0
-let temp_msb = 0
-let I2C_ADDRESS = 68
-let TEMP_CMD = 44
-let HUMIDITY_CMD = 44
-let READ_REG = 231
+const I2C_ADDRESS = 0x44;
+const SHT31_MEAS_HIGHREP = 0x2400;
+let humidity_checksum = 0;
+let len = 2;
+
+//% color=#4c6ef5 weight=25 icon="\uf043" block="SHT3x Sensor"
 namespace CipSHT3xDriver {
 
-    pins.i2cWriteNumber(I2C_ADDRESS, TEMP_CMD, NumberFormat.UInt32BE, false);
+    pins.i2cWriteNumber(I2C_ADDRESS, SHT31_MEAS_HIGHREP, NumberFormat.UInt8BE, false);
     basic.pause(20);
-    temp_msb = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
-    temp_lsb = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
-    temp_checksum = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
+    let buff = pins.i2cReadBuffer(I2C_ADDRESS, 6);
 
-    // Enviar comando para leer la humedad
-    pins.i2cWriteNumber(I2C_ADDRESS, HUMIDITY_CMD, NumberFormat.UInt32BE, false);
-    basic.pause(20);
-    humidity_msb = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
-    humidity_lsb = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
-    humidity_checksum = pins.i2cReadNumber(I2C_ADDRESS, NumberFormat.UInt32BE, true);
-    /**
-     * Read Relative Humidity from the SHT3x Sensor.
-     * Returns a number describing the relative humidity in percentage % relative
-     * humidity
-    */
-    //% blockId="SHT3x_read_humidity"
-    //% block="Leer Humedad"
-    export function read_humidity(): number {
-        let humidity = (((humidity_msb * 256) + humidity_lsb) * 100) / 65535.0;
-        return humidity;
+    function crc8(lsb = NumberFormat.UInt8BE, msb = NumberFormat.UInt8BE, cr = NumberFormat.UInt8LE) {
+        const poly = 0x31;
+        let crc = 0XFF;
+        crc ^= msb;
+
+        for (let i = 0; i < 8; i++)
+            crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+
+        crc ^= lsb;
+        for (let i = 0; i < 8; i++)
+            crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+
+        if (crc == cr) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    /**
-     * Read Temperature in degrees celcius from the SHT3x sensor.
-     * Returns a number describing the ambient temperature in degrees celsius
-    */
-    //% blockId="SHT3xDriver_read_temperature"
-    //% block="Leer Temperatura"
-    export function read_temperature(): number {
-        let temperature = ((((temp_msb * 256) + temp_lsb) * 175) / 65535.0) - 45;
-        return temperature;
+    if ((crc8(buff[0], buff[1], buff[2])) && (crc8(buff[3], buff[4], buff[5]))) {
+        let temp = (buff[0] << 8) + (buff[1] << 0);
+        let relHum = (buff[3] << 8) + (buff[4] << 0);
+        let temperature = (temp) * 0.00267033 - 45;
+        let relHumidity = (relHum) * 0.0015259;
     }
 
 }
